61c49e3b1ee5f7966e1cc826723f9c1a413ae9bd
Artem Andriushchenko <link.by.artem@gmail.com>
2024-06-09 20:01:04 +0300

    Register Borrowing models in the admin.py

--- borrowings/admin.py
+++ borrowings/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from borrowings.models import Borrowing
+
+
+@admin.register(Borrowing)
+class BorrowingAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "borrow_date",
+        "actual_return_date",
+        "book",
+        "user",
+    )
+    search_fields = ("id", "user__email", "book__title")
+    ordering = (
+        "-borrow_date",
+        "actual_return_date",
+    )
