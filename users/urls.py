f56ffdf427c0d24d17e3d1ecd4bfc55e5df36850
Artem Andriushchenko <link.by.artem@gmail.com>
2024-06-09 15:44:18 +0300

    Add views, urls to users & urlpatterns to library_service_api

---
+++ users/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from users.views import ManageUserView, CreateUserView
+
+urlpatterns = [
+    path("", CreateUserView.as_view(), name="register"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("me/", ManageUserView.as_view(), name="manage"),
+]
+
+app_name = "user"
--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+from users.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = (JWTAuthentication,)
+
+    def get_object(self):
+        return self.request.user
