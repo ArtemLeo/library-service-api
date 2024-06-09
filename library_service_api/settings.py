a74bbf0259371d68e9c38428fb86631729be2203
Artem Andriushchenko <link.by.artem@gmail.com>
2024-06-09 16:04:02 +0300

    Removed BLACKLIST_AFTER_ROTATION from SIMPLE_JWT settings.py

--- library_service_api/settings.py
+++ library_service_api/settings.py
@@ -135,6 +135,5 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=45),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
-    "BLACKLIST_AFTER_ROTATION": True,
     "AUTH_HEADER_NAME": "AUTHORIZE",
 }
