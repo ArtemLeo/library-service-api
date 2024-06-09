2a9bd814eebfefb20954a75f91ac8dcb866cec5c
Artem Andriushchenko <link.by.artem@gmail.com>
2024-06-09 16:32:13 +0300

    Add Meta class to borrowings/models.py

--- borrowings/models.py
+++ borrowings/models.py
@@ -17,3 +17,29 @@ class Borrowing(models.Model):
         on_delete=models.CASCADE,
         related_name="borrowings",
     )
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=Q(borrow_date__gte=timezone.now().date()),
+                name="borrow_date_not_past",
+            ),
+            models.CheckConstraint(
+                check=(
+                        Q(expected_return_date__gte=F("borrow_date"))
+                        & Q(actual_return_date__gte=F("borrow_date"))
+                ),
+                name="return_date_gte_borrow_date",
+            ),
+            models.CheckConstraint(
+                check=Q(
+                    expected_return_date__lte=(
+                            F("borrow_date") + timedelta(weeks=2)
+                    )
+                ),
+                name="expected_return_date_within_two_weeks",
+            ),
+        ]
+
+    def __str__(self):
+        return f"{self.book} borrowed by {self.user}"
