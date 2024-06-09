6b7f2057b99558eb6dda42f4f0e38039ac9e2419
Artem Andriushchenko <link.by.artem@gmail.com>
2024-06-09 15:19:36 +0300

    Implement BookViewSet & Add urlpatterns for book

--- books/views.py
+++ books/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from books.models import Book
+from books.serializers import BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
