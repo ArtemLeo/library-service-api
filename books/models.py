c7e0ebdb1fecbc682ce754fb639b10ac071aa63f
Artem Andriushchenko <link.by.artem@gmail.com>
2024-06-09 17:04:49 +0300

    Changed books/models.py __str__ method by adding num of available books

--- books/models.py
+++ books/models.py
@@ -13,4 +13,4 @@ class Book(models.Model):
     daily_fee = models.DecimalField(max_digits=8, decimal_places=2)
 
     def __str__(self):
-        return f"{self.title} by {self.author}"
+        return f"{self.title} by {self.author} ({self.inventory} available)"
