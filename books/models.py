3bd12563267fdeeac9ff21796eb3f87afa32323d
Artem Andriushchenko <link.by.artem@gmail.com>
2024-06-09 11:04:23 +0300

    Add models to books app

--- books/models.py
+++ books/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    class Cover(models.TextChoices):
+        SOFT = "1", "SOFT"
+        HARD = "2", "HARD"
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(choices=Cover.choices, max_length=2)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
