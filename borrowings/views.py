b5954fa08ad414b2542fe5caa9be65bb5caee20a
Artem Andriushchenko <link.by.artem@gmail.com>
2024-06-09 19:31:16 +0300

    Integrate sending notifications on new borrowing creation

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,5 +1,3 @@
-from drf_spectacular.types import OpenApiTypes
-from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import mixins, status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
@@ -11,6 +9,7 @@ from borrowings.serializers import (
     BorrowingDetailSerializer,
     BorrowingCreateSerializer,
 )
+from telegram_api.telegram_helper import telegram_helper
 
 
 class BorrowingViewSet(
@@ -43,10 +42,24 @@ class BorrowingViewSet(
             user_id = self.request.query_params.get("user_id")
             if user_id:
                 queryset = queryset.filter(user_id=user_id)
+
         return queryset
 
+    @staticmethod
+    def notify_borrowing(borrowing):
+        message = (
+            f"📚 *New Borrowing Created* \n\n"
+            f"*Borrowing ID:*   {borrowing.pk}\n"
+            f"*Borrowing Date:*   {borrowing.borrow_date}\n"
+            f"*Expected Return Date:*   {borrowing.expected_return_date}\n"
+            f"*Book Title:*   {borrowing.book.title}\n"
+            f"*Book Author:*   {borrowing.book.author}"
+        )
+        telegram_helper.send_message(message)
+
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        borrowing = serializer.save(user=self.request.user)
+        self.notify_borrowing(borrowing)
 
     @action(
         methods=["POST"],
@@ -65,22 +78,4 @@ class BorrowingViewSet(
         serializer = self.get_serializer(borrowing, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-
         return Response(serializer.data, status=status.HTTP_200_OK)
-
-    @extend_schema(
-        parameters=[
-            OpenApiParameter(
-                "is_active",
-                type=OpenApiTypes.BOOL,
-                description="Filter by actual return date (ex. ?is_active=True)",
-            ),
-            OpenApiParameter(
-                "user_id",
-                type=OpenApiTypes.INT,
-                description="Filter by user id (ex. ?user_id=2)",
-            ),
-        ]
-    )
-    def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
