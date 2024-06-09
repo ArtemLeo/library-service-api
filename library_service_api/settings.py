805f20b7be1dded0ae70375ec2ec4999fdc2279a
Artem Andriushchenko <link.by.artem@gmail.com>
2024-06-09 16:00:28 +0300

    Add DEFAULT_PERMISSION_CLASSES & Import timedelta to settings.py

--- library_service_api/settings.py
+++ library_service_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,7 +125,10 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ]
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "books.permissions.IsAdminOrReadOnly",
+    ],
 }
 
 SIMPLE_JWT = {
